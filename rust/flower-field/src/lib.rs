533565797693075decd82d8a8eea18caf0b1917c
Hayk Hakobyan <hayk.hakobyan87@gmail.com>
2025-08-30 21:23:06 -0400

    flower-field - 2

--- rust/flower-field/src/lib.rs
+++ rust/flower-field/src/lib.rs
@@ -6,42 +6,53 @@ pub fn annotate(garden: &[&str]) -> Vec<String> {
     if garden.is_empty() {
         return Vec::new();
     }
+    if garden[0].is_empty() {
+        return vec![String::new()];
+    }
 
-    let mut ret = Vec::new();
+    let rows = garden.len();
+    let cols = garden[0].len();
 
-    let (rows, cols) = (garden.len() as i32, garden.first().unwrap().len() as i32);
-    for row in 0..rows {
-        let mut ss = String::new();
-        for col in 0..cols {
-            if let Some(ch) = garden[row as usize].chars().nth(col as usize) {
-                if ch == '*' {
-                    ss.push('*');
-                } else {
-                    let mut count = 0;
-                    for rr in (row - 1)..=(row + 1) {
-                        for cc in (col - 1)..=(col + 1) {
-                            if rr == row && cc == col {
-                                continue;
-                            }
-                            if let Some(&ri) = garden.get(rr as usize) {
-                                if let Some(ch) = ri.chars().nth(cc as usize) {
-                                    if ch == '*' {
-                                        count += 1;
-                                    }
-                                }
-                            }
+    (0..rows)
+        .map(|row| {
+            (0..cols)
+                .map(|col| {
+                    if garden[row].as_bytes()[col] == b'*' {
+                        '*'
+                    } else {
+                        match count_neighbour_flowers(garden, row, col) {
+                            0 => ' ',
+                            count => char::from_digit(count.into(), 10).unwrap(),
                         }
                     }
-                    if count == 0 {
-                        ss.push(' ');
-                    } else {
-                        ss.push(char::from_digit(count, 10).unwrap());
-                    }
+                })
+                .collect()
+        })
+        .collect()
+}
+
+fn count_neighbour_flowers(garden: &[&str], row: usize, col: usize) -> u8 {
+    let rows = garden.len() as isize;
+    let cols = garden[0].len() as isize;
+
+    let mut count = 0;
+
+    for dr in -1..=1 {
+        for dc in -1..=1 {
+            if dr == 0 && dc == 0 {
+                continue;
+            }
+
+            let new_row = row as isize + dr;
+            let new_col = col as isize + dc;
+
+            if new_row >= 0 && new_row < rows && new_col >= 0 && new_col < cols {
+                if garden[new_row as usize].as_bytes()[new_col as usize] == b'*' {
+                    count += 1;
                 }
             }
         }
-        ret.push(ss);
     }
 
-    ret
+    count
 }
